2dedcb84981839d8258a59e77e4060c47c72ca23
david <david@LuvMackio>
2025-03-20 20:49:34 -0400

    final question 5

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question5/Streamable.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.midterm.question5;
+
+/**
+ * Shared behaviors
+ * Defines play, pause, and stop functionalities.
+ * 
+ * @author David Mack
+ */
+public interface Streamable {
+    void play();
+    void pause();
+    void stop();
+}
